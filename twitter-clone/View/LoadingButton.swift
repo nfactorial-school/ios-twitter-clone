94afdfd51fd4b513bb3af8f56a61016fb7bec985
Aidar Nugmanoff <a.nugmanoff@gmail.com>
2020-08-07 03:29:02 +0600

    refactor: LoadingButton refactoring

--- twitter-clone/View/LoadingButton.swift
+++ twitter-clone/View/LoadingButton.swift
@@ -10,43 +10,34 @@ import UIKit
 
 class LoadingButton: UIButton {
     var originalButtonText: String?
-    var activityIndicator: UIActivityIndicatorView!
     
-    func showLoading() {
-        originalButtonText = self.titleLabel?.text
-        self.setTitle("", for: .normal)
-        
-        if (activityIndicator == nil) {
-            activityIndicator = createActivityIndicator()
-        }
-        
-        showSpinning()
-    }
-    
-    func hideLoading() {
-        self.setTitle(originalButtonText, for: .normal)
-        activityIndicator.stopAnimating()
-    }
-    
-    private func createActivityIndicator() -> UIActivityIndicatorView {
+    lazy var activityIndicator: UIActivityIndicatorView = {
         let activityIndicator = UIActivityIndicatorView()
         activityIndicator.hidesWhenStopped = true
         activityIndicator.color = .white
         return activityIndicator
+    }()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setup()
     }
     
-    private func showSpinning() {
+    func setup() {
         activityIndicator.translatesAutoresizingMaskIntoConstraints = false
-        self.addSubview(activityIndicator)
-        centerActivityIndicatorInButton()
+        addSubview(activityIndicator)
+        activityIndicator.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        activityIndicator.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+    }
+    
+    func showLoading() {
+        originalButtonText = titleLabel?.text
+        setTitle("", for: .normal)
         activityIndicator.startAnimating()
     }
     
-    private func centerActivityIndicatorInButton() {
-        let xCenterConstraint = NSLayoutConstraint(item: self, attribute: .centerX, relatedBy: .equal, toItem: activityIndicator, attribute: .centerX, multiplier: 1, constant: 0)
-        self.addConstraint(xCenterConstraint)
-        
-        let yCenterConstraint = NSLayoutConstraint(item: self, attribute: .centerY, relatedBy: .equal, toItem: activityIndicator, attribute: .centerY, multiplier: 1, constant: 0)
-        self.addConstraint(yCenterConstraint)
+    func hideLoading() {
+        setTitle(originalButtonText, for: .normal)
+        activityIndicator.stopAnimating()
     }
 }
